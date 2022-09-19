3b894737e8221daf446de8b3525710ec3e8ab07f
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-09-19 11:18:11 -0300

    começando exercicio fixação

---
+++ exercise/fixation-exercises-10-2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VideoGameList from "./components/VideoGameList";
+
+function App() {
+  return (
+    <div className="App"> 
+    <VideoGameList/>
+     </div>
+  )
+}
+
+export default App;
---
+++ exercise/fixation-exercises-10-2/src/components/VideoGameList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class VideoGameList extends React.Component {
+    render() {
+        return (
+            <h1>Lista de jogos mais vendidos</h1>
+        )
+    }
+}
+
+export default VideoGameList;
