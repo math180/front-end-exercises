2e4547c34d1c3a9d9e43940f20f2ee98d2cdb512
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-09-22 15:17:40 -0300

    criando função que encontra o elemento que é igual ao digitado no input

--- exercise/exercises/src/App.jsx
+++ exercise/exercises/src/App.jsx
@@ -8,16 +8,28 @@ class App extends React.Component {
 
   constructor() {
     super()
+
+    this.setFilterValue = this.setFilterValue.bind(this); // criando o bind aqui
+
     this.state = {
         filter: 'matheus'
     }
 }
 
-    setFilterValue(event) {
-      return (event.target.value);
+    setFilterValue(event) { // se esta função for uma arrow func. ñ é necessário criar o bind
+     this.setState({
+        filter: event.target.value // precisa fazer o bind no constructor para funcionar 
+     });
     }
 
   render() {
+
+    const { filter } = this.state;
+
+    const filteredColors = colorList.filter((color) => { // criação da funçao que filtrará o valor digitado no input com o valor que tem na propriedade filter
+        const colorItem = color.color;
+        return colorItem.includes(filter)
+    });
 
     const createListLi = colorList.map((color) => {
         return <li key={color.number}>
